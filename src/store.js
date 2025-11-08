3d5428290296d1464ca976e95242405a4e5111b8
theia-joshua <theia@skillsnetworklabs.com>
2025-11-08 17:26:17 -0500

    Complete Task 5: configure Redux store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
+// src/store.js
+
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store using configureStore from Redux Toolkit
+const store = configureStore({
+  reducer: {
+    // 'cart' is the slice name, managed by cartReducer
+    cart: cartReducer,
+  },
 });
-export default store
+
+export default store;
